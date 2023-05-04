1d0d2af9b5cf42e108cf261440f1edb710fd2b77
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-05-04 14:19:40 +0300

    Create method to calculate the value

--- manager.rb
+++ manager.rb
@@ -1,6 +1,7 @@
 require "uri"
 require "net/http"
 require "json"
+require "./coin"
 
 class Manager
  @@repo = {}
@@ -9,7 +10,7 @@ class Manager
  end
 
  def initialize_repo
-  response web_scrap
+  response = web_scrap
   json = JSON.parse(response)
   for symbol, values in json 
    coin = Coin.new(symbol, values['USD'], values['EUR'])
@@ -17,9 +18,19 @@ class Manager
   end
  end
 
- def web_scrap
-  url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,DASH,LTC&tsyms=USD,EUR'
-  uri = URI(url)
-  Net::HTTP.get(Url)
+  def web_scrap
+    url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,DASH,LTC&tsyms=USD,EUR'
+    uri = URI(url)
+    Net::HTTP.get(uri)
+  end
+
+
+ def coin_list
+  @@repo.keys
  end
+
+  def calculate(amount, symbol, to)
+   coin = @@repo[symbol]
+   amount*coin.send(to.to_sym)
+  end
 end
